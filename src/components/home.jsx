cb140713fbbaa152e93dceb472c5a13115c962dc
gaurav <gaurav@diatoz.lan>
2018-10-23 20:24:48 +0530

    git push

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 class Home extends Component {
-    state = {  }
-    render() { 
-        return (      
-             <h1>This is Home Page</h1>
-            );
-    }
+  state = {
+    persons: [],
+    name:'',
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>
+          {this.props.title} {this.props.name}
+        </h1>
+        <ul>
+          {this.state.persons.map(person => (<li>{person.name}</li>))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
